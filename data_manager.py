61ee13f80c03f26385003f683dc4a653ea2afe9d
dagggy <krzysiekztubow@gmail.com>
2021-12-14 11:14:19 +0100

    log in complete

--- data_manager.py
+++ data_manager.py
@@ -147,7 +147,6 @@ def get_tag_id_by_tag_bd(cursor, tag):
     return cursor.fetchall()
 
 
-
 @database_common.connection_handler
 def get_tag_id_by_question_id_bd(cursor, question_id):
     cursor.execute(f"""
@@ -158,6 +157,14 @@ def get_tag_id_by_question_id_bd(cursor, question_id):
     return cursor.fetchall()
 
 
+@database_common.connection_handler
+def get_password_by_email(cursor, email):
+    cursor.execute(f"""
+                        SELECT password
+                        FROM user_data
+                        WHERE login = '{email}'
+    """)
+    return cursor.fetchone()
 
 
 ################################################
