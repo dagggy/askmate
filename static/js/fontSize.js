9744fca0da2024c58a9222e450ad6754cdae5cd0
dagggy <krzysiekztubow@gmail.com>
2021-12-14 21:55:19 +0100

    font size working... though scuffed as hell requirements: - import script - copy and paste buttons - change the buttons to target whatever element you want - give the element an ID and style='font-size: some px, probably 12'

--- static/js/fontSize.js
+++ static/js/fontSize.js
@@ -1,15 +1,19 @@
-const el = document.getElementsByName('body');
-const style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-const fontSize = parseFloat(style);
-
-function enlargeFont(){
-    if(el.style.fontSize <15){
-        el.style.fontSize = (fontSize + 1) + 'px';
+function enlargeFont(className){
+    let txt = document.getElementById(className);
+    let style = window.getComputedStyle(txt, null).getPropertyValue('font-size')
+    let currentSize = parseFloat(style);
+    console.log(currentSize)
+    if(currentSize < 15){
+        txt.style.fontSize = (currentSize + 1) + 'px'
     }
 }
 
-function ensmallenFont(){
-    if(el.style.fontSize <3){
-        el.style.fontSize = (fontSize - 1) + 'px';
+function ensmallenFont(className){
+    let txt = document.getElementById(className);
+    let style = window.getComputedStyle(txt, null).getPropertyValue('font-size')
+    let currentSize = parseFloat(style);
+    console.log(currentSize)
+    if(currentSize > 3){
+        txt.style.fontSize = (currentSize - 1) + 'px'
     }
 }
